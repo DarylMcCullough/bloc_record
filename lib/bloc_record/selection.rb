66b69a24f463078f6cb329309eeb57400dc95451
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-12-09 15:32:58 +0000

    finished implementing join

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -127,7 +127,145 @@ module Selection
         end until arr.length < batch_size
     end
     
+    def where(*args)
+        if args.count > 1
+            expression = args.shift
+            params = args
+        else
+            case args.first
+            when String
+                expression = args.first
+            when Hash
+                expression_hash = BlocRecord::Utility.convert_keys(args.first)
+                expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+            end
+        end
+
+        sql = <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+            WHERE #{expression};
+        SQL
+
+        rows = connection.execute(sql, params)
+        rows_to_array(rows)
+    end
+
+    
+    def order(*args, **kwargs)
+        arglist = []
+        args.each do |arg|
+            case arg
+            when String # either "name" or "name asc" or ...
+                parts = arg.split()
+                begin
+                    key = parts[0] # for example, "name"
+                    direction = get_order_direction(parts[1..-1]) # interpret the rest of the string as a direction, "asc" or "desc"
+                    value = key + " " + direction
+                    arglist.push(value)
+                rescue
+                    raise ArgumentError.new("the argument to 'order' not understood: #{arg}")
+                end
+            when Symbol
+                key = arg.to_s
+                direction = 'ASC' # the default
+                value = key + " " + direction
+                arglist.push(value)
+                next
+            end
+            raise ArgumentError.new("the argument to 'order' not understood: #{arg}")
+        end
+        kwargs.each do |key, value| # pairs of the form name: :asc, etc.
+            key = key.to_s
+            direction = get_order_direction([value])
+            value = key + " " + direction
+            arglist.push(value)
+            next
+        end
+        order = arglist.join(",")
+
+        rows = connection.execute <<-SQL
+            SELECT * FROM #{table}
+            ORDER BY #{order};
+        SQL
+        rows_to_array(rows)
+    end
+    
+    def join(*args, **kwargs)
+        # The first args are interpreted as table names.
+        # It is assumed that each table that uses foreign keys
+        # has a key of the form <foreign_table>_id.
+        # So we are joining the tables on <this_table>.id = <that_table>.<this_table>_id
+        
+        # The kwargs are interpreted as pairs of table names.
+        # We are joing the first with <this_table> on <this_table>.id = <first_table>.<this_table>_id
+        # But we are joiningthe second with <first_table>.id = <second_table>.<first_table>_id
+        
+        if args.count + kwargs.count > 1
+            joins = get_join_statement(table, *args, **kwargs) #see below
+            rows = connection.execute <<-SQL
+                SELECT * FROM #{table} #{joins}
+            SQL
+        else
+            case args.first
+            when String
+                rows = connection.execute <<-SQL
+                    SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+                SQL
+            when Symbol
+                rows = connection.execute <<-SQL
+                    SELECT * FROM #{table}
+                    INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+                SQL
+            end
+        end
+ 
+        rows_to_array(rows)
+    end
+    
     private
+    
+    def get_join_statement(table, *args, **kwargs)
+        # The first args are interpreted as table names.
+        # It is assumed that each table that uses foreign keys
+        # has a key of the form <foreign_table>_id.
+        # So we are joining the tables on <this_table>.id = <that_table>.<this_table>_id
+        
+        # The kwargs are interpreted as pairs of table names.
+        # We are joing the first with <this_table> on <this_table>.id = <first_table>.<this_table>_id
+        # But we are joiningthe second with <first_table>.id = <second_table>.<first_table>_id
+        
+        join_ids = {}
+        args.each do |arg|
+            join_ids[arg] = table
+        end 
+
+        kwargs.each do |table1, table2|
+            join_ids[table1] = table
+            join_ids[table2] = table1
+        end
+        
+		result = join_ids.map do |table_name, table_to_join|
+		    "INNER JOIN #{table_name} ON #{table_name}.#{table_to_join}_id = #{table_to_join}.id"
+		end
+		result = result.join(" ")
+        return result
+    end
+    
+    ##
+    
+    def get_order_direction(args)
+        if args.length == 0
+            return 'ASC' # no arguments, use the default
+        end
+        if args.length == 1 # one argument, should be asc or desc
+            arg = args[0].to_s.upcase
+            if ['ASC', 'DESC'].include? arg
+                return arg
+            end
+        end
+        raise ArgumentError.new("not a direction: #{args.join(' ')}")
+    end
+    
     def init_object_from_row(row)
         if row
             data = Hash[columns.zip(row)]
@@ -138,7 +276,7 @@ module Selection
     def rows_to_array(rows)
         rows.map { |row| new(Hash[columns.zip(row)]) }
     end
-
+    
     def method_missing(m, *args, &block)
         m = m.to_s # m will be a symbol until coerced to a stringd
         # we want to handle the following case:
