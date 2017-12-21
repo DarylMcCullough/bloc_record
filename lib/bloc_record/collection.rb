d2570186f5c744ac11ac024360b58e6ebf94db82
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-12-21 03:08:40 +0000

    add destroy_all to Collection

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -5,8 +5,49 @@ module BlocRecord
             self.any? ? self.first.class.update(ids, updates) : false
         end
         
-        def take()
-            self.any? ? self.first : nil
+        def take(n=1)
+            if not(n.is_a? Integer) || n < 1
+                raise ArgumentError.new("take: argument must be a positive integer")
+            end
+            return self.sample(n)
+        end
+        
+        def destroy_all(*args)
+            if self.count == 0
+                return
+            end
+            
+            if args.count == 0
+                self.each do |entry|
+                    entry.destroy()
+                end
+                return
+            end
+            if args.count > 1
+                expression = args.shift
+                id_expression = self.get_id_expression()
+                expression = "(#{expression}) AND (#{id_expression})"
+                params = args
+                self.first.class.destroy_all(expression, *params)
+                return
+            else
+                case args.first
+                when String
+                    expression = args.first
+                    id_expression = self.get_id_expression()
+                    expression = "(#{expression}) AND (#{id_expression})"
+                    self.first.class.destroy_all(expression)
+                    return
+                when Hash
+                    hash = args.first
+                    self.each do |entry|
+                        hash["id"] = entry.send("id")
+                    end
+                    self.first.class.destroy_all(hash)
+                    return
+                end
+            end
+            raise ArgumentError.new("destroy_all: arguments must be strings or hashes")
         end
         
         def where(**kwargs)
@@ -26,6 +67,12 @@ module BlocRecord
             end
         end
         
+        def destroy_all()
+            self.each do |record|
+                record.destroy()
+            end
+        end
+        
         def not(**kwargs)
             BlocRecord::Utility.convert_keys(kwargs)
             retval = []
@@ -41,6 +88,13 @@ module BlocRecord
                     retval.append(record)
                 end
             end
+        end
+        
+        private
+        def get_id_expression()
+            ids = self.map { |entry| entry.send("id") }
+            expression = 'id IN (#{ids.join(",")})'
+            return expression
         end
     end
 end
